3b3dd69337d6030c76aecd6e2b09b7b218f2002d
Jose De la Cruz <jose@delacruzhome.com>
2025-04-29 09:24:17 -0400

    test: enhance repository and test structure functionality
    - Added `AddRangeAsync` method to `RepositoryBase` and `IRepository` for batch entity addition.-
    Restored `Dispose` methods for proper resource management.- Renamed `FeatureHook` to `TestRunHook`
    with updated attributes and methods for test run context.- Refactored
    `AntecedentServiceStepDefinitions` to include service initialization and implement pending steps.

--- ABC.PostgreSQL.Tests/StepDefinitions/AntecedentServiceStepDefinitions.cs
+++ ABC.PostgreSQL.Tests/StepDefinitions/AntecedentServiceStepDefinitions.cs
@@ -1,27 +1,54 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ABC.Management.Domain.Entities;
+using ABC.PostGreSQL;
+using ABC.PostGreSQL.ValidationServices;
+using FluentAssertions;
+using Microsoft.Extensions.DependencyInjection;
 using Reqnroll;
 
-namespace ABC.PostgreSQL.Tests.StepDefinitions
+namespace ABC.PostgreSQL.Tests.StepDefinitions;
+
+[Binding]
+public class AntecedentServiceStepDefinitions
 {
-    [Binding]
-    public class AntecedentServiceStepDefinitions
+    private readonly AntecedentService _sut;
+    private string _expectedName = string.Empty;
+    private Antecedent? _actual;
+
+    private IEnumerable<Antecedent> _antecedents = 
+        Enumerable.Range(0, 5)
+        .Select(i => new Antecedent(
+            Guid.NewGuid(), 
+            $"Antecedent{i}",
+            Faker.Lorem.Sentence(1)));
+
+
+    public AntecedentServiceStepDefinitions()
     {
-        [Given("I have a name found in the antecedent table")]
-        public void GivenIHaveANameFoundInTheAntecedentTable()
-        {
-            throw new PendingStepException();
-        }
+        var uow = StartupFixture.Instance.Services
+            .GetRequiredService<UnitOfWork>();
 
-        [Given("I make a call to the antecedent service")]
-        public void GivenIMakeACallToTheAntecedentService()
-        {
-            throw new PendingStepException();
-        }
+        Action action = async () =>
+            await uow.Antecedents.AddRangeAsync(_antecedents);
 
-        [Then("I should receive the antecedent object from the database")]
-        public void ThenIShouldReceiveTheAntecedentObjectFromTheDatabase()
-        {
-            throw new PendingStepException();
-        }
+        action.Invoke();
+        _ = uow.SaveChangesAsync().Result;
+
+        _sut = new AntecedentService(uow);
     }
+
+    [Given("I have a name found in the antecedent table")]
+    public void GivenIHaveANameFoundInTheAntecedentTable() =>
+        _expectedName = _antecedents.First().Name;
+
+    [Given("I make a call to the antecedent service")]
+    public async Task GivenIMakeACallToTheAntecedentService() =>
+        _actual = await _sut.GetByName(_expectedName);
+
+    [Then("I should receive the antecedent object from the database")]
+    public void ThenIShouldReceiveTheAntecedentObjectFromTheDatabase() =>
+        _actual.Should().NotBeNull();
 }
