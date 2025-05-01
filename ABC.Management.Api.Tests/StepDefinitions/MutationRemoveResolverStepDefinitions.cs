fff10d58b505c20489b8e9323a537e9614a55c25
Jose De la Cruz <jose.delacruz@microsoft.com>
2025-05-01 17:54:36 -0400

    test: expanded `Mutation.feature` with scenarios for creating commands
    - Added `Shouldly` package for expressive assertions in tests.
    - Introduced `MutationRemoveResolver.feature` for removal scenarios.
    - Corrected typo in `Query.feature` for consistent phrasing.
    - Updated package versions in `Directory.Packages.props`.

---
+++ ABC.Management.Api.Tests/StepDefinitions/MutationRemoveResolverStepDefinitions.cs
@@ -0,0 +1,106 @@
+using ABC.Management.Api.Commands;
+using ABC.Management.Api.Types;
+using FakeItEasy;
+using HotChocolate.Resolvers;
+using Mediator;
+using Microsoft.Extensions.DependencyInjection;
+using Reqnroll;
+using Shouldly;
+
+namespace ABC.Management.Api.Tests.StepDefinitions;
+
+[Binding]
+public class MutationRemoveResolverStepDefinitions
+{
+    private readonly IMediator _mediatorFake;
+    private readonly IResolverContext _resolverContext;
+
+    private Guid _existingGuid;
+    private readonly MutationRemoveResolvers _sut;
+    private bool _actual = false;
+
+    public MutationRemoveResolverStepDefinitions(StartupFixture fixture)
+    {
+        _mediatorFake = fixture.Services.GetRequiredService<IMediator>();
+        _resolverContext = fixture.Services.GetRequiredService<IResolverContext>();
+        A.CallTo(() => _resolverContext.HasErrors).Returns(false);
+        _sut = new MutationRemoveResolvers();
+    }
+
+    [Given("I have a valid RemoveAntecedentCommand request")]
+    public void GivenIHaveAValidRemoveAntecedentCommandRequest() =>
+        _existingGuid = Guid.NewGuid();
+
+    [When("I send the request to delete the antecedent")]
+    public async Task WhenISendTheRequestToDeleteTheAntecedent() =>
+        _actual = await _sut.RemoveAntecedent(_mediatorFake, _existingGuid, _resolverContext);
+
+
+    [Then("I should send a request to the RemoveAntecedentCommand handler")]
+    public void ThenIShouldSendARequestToTheRemoveAntecedentCommandHandler()
+    {
+        A.CallTo(() => _mediatorFake.Send(
+            A<RemoveAntecedentCommand>.That.Matches(a => a.Entity.Id.Equals(_existingGuid)),
+            A<CancellationToken>._))
+        .MustHaveHappenedOnceExactly();
+
+        _actual.ShouldBeTrue();
+    }
+
+    [Given("I have a valid RemoveBehaviorCommand request")]
+    public void GivenIHaveAValidRemoveBehaviorCommandRequest() =>
+        _existingGuid = Guid.NewGuid();
+
+    [When("I send the request to delete the behavior")]
+    public async Task WhenISendTheRequestToDeleteTheBehavior() =>
+        _actual = await _sut.RemoveBehavior(_mediatorFake, _existingGuid, _resolverContext);
+
+    [Then("I should send a request to the RemoveBehaviorCommand handler")]
+    public void ThenIShouldSendARequestToTheRemoveBehaviorCommandHandler()
+    {
+        A.CallTo(() => _mediatorFake.Send(
+           A<RemoveBehaviorCommand>.That.Matches(a => a.Entity.Id.Equals(_existingGuid)),
+           A<CancellationToken>._))
+       .MustHaveHappenedOnceExactly();
+
+        _actual.ShouldBeTrue();
+    }
+
+    [Given("I have a valid RemoveConsequenceCommand request")]
+    public void GivenIHaveAValidRemoveConsequenceCommandRequest() =>
+        _existingGuid = Guid.NewGuid();
+
+    [When("I send the request to delete the consequence")]
+    public async Task WhenISendTheRequestToDeleteTheConsequence() =>
+        _actual = await _sut.RemoveConsequence(_mediatorFake, _existingGuid, _resolverContext);
+
+    [Then("I should send a request to the RemoveConsequenceCommand handler")]
+    public void ThenIShouldSendARequestToTheRemoveConsequenceCommandHandler()
+    {
+        A.CallTo(() => _mediatorFake.Send(
+           A<RemoveConsequenceCommand>.That.Matches(a => a.Entity.Id.Equals(_existingGuid)),
+           A<CancellationToken>._))
+       .MustHaveHappenedOnceExactly();
+
+        _actual.ShouldBeTrue();
+    }
+
+    [Given("I have a valid RemoveChildCommand request")]
+    public void GivenIHaveAValidRemoveChildCommandRequest() =>
+        _existingGuid = Guid.NewGuid();
+
+    [When("I send the request to delete the child")]
+    public async Task WhenISendTheRequestToDeleteTheChild() =>
+        _actual = await _sut.RemoveChild(_mediatorFake, _existingGuid, _resolverContext);
+
+    [Then("I should send a request to the RemoveChildCommand handler")]
+    public void ThenIShouldSendARequestToTheRemoveChildCommandHandler()
+    {
+        A.CallTo(() => _mediatorFake.Send(
+           A<RemoveChildCommand>.That.Matches(a => a.Entity.Id.Equals(_existingGuid)),
+           A<CancellationToken>._))
+       .MustHaveHappenedOnceExactly();
+
+        _actual.ShouldBeTrue();
+    }
+}
